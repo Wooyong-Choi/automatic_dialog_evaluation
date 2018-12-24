d4921d38c72d54ce3857aa8a1683ac2ab2b8bae6
sjchoi <sjchoi@sejong.knu.ac.kr>
2018-12-24 02:32:07 +0000

    fix2

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import sys
 import argparse
+from trainer import *
 
 parser = argparse.ArgumentParser('[*] Argument ')
 
-parser.add_argument('-train', help='help')
+parser.add_argument('-train',  help='train')
+
+parser.add_argument('-device', help = 'GPU number')
+parser.add_argument('-dim',    help = 'embedding dimenstion size')
+parser.add_argument('-layer',  help = '# of layer')
+parser.add_argument('-batch',  help = '# of batch')
+parser.add_argument('-hidden', help = '# of hidden')
+parser.add_argument('-margin', help = 'margin')
+parser.add_argument('-epoch',  help = '# of epoch')
+parser.add_argument('-lr',     help = 'learning rate')
+
+parser.add_argument('-data',   help = 'data folder path')
+parser.add_argument(''
+
+parser.add_argument('-test', help = 'test')
+
 args = parser.parse_args()
 
 print(args.train)
