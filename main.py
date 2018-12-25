06268ae33d81af87158926f364c7107df1936a40
sjchoi <sjchoi@sejong.knu.ac.kr>
2018-12-25 12:28:19 +0000

    add_argument

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import sys
 import argparse
-from trainer import *
+from trainer import train, test
 
 parser = argparse.ArgumentParser('[*] Argument ')
 
-parser.add_argument('-train',  help='train')
+parser.add_argument('-train', default = 'true', help = 'train True or False')
+parser.add_argument('-test',  default = 'false',help = 'test True or False')
 
-parser.add_argument('-device', help = 'GPU number')
-parser.add_argument('-dim',    help = 'embedding dimenstion size')
-parser.add_argument('-layer',  help = '# of layer')
-parser.add_argument('-batch',  help = '# of batch')
-parser.add_argument('-hidden', help = '# of hidden')
-parser.add_argument('-margin', help = 'margin')
-parser.add_argument('-epoch',  help = '# of epoch')
-parser.add_argument('-lr',     help = 'learning rate')
+parser.add_argument('-device', default = 3,     help = 'GPU number')
+parser.add_argument('-dim',    default = 150,   help = 'embedding dimenstion size')
+parser.add_argument('-layer',  default = 1,     help = '# of layer')
+parser.add_argument('-batch',  default = 1028,  help = '# of batch')
+parser.add_argument('-hidden', default = 512,   help = '# of hidden')
+parser.add_argument('-margin', default = 1,     help = 'margin')
+parser.add_argument('-epoch',  default = 50,    help = '# of epoch')
+parser.add_argument('-lr',     default = 0.001, help = 'learning rate')
 
-parser.add_argument('-data',   help = 'data folder path')
-parser.add_argument(''
-
-parser.add_argument('-test', help = 'test')
+parser.add_argument('-data',   default = './dataset/',    help = 'data folder path')
+parser.add_argument('-pretrain',default= './sample_onmt/',help = 'pretrain model path')
+parser.add_argument('-output', default = './result/model',help = 'output path')
 
 args = parser.parse_args()
 
-print(args.train)
+if args.train == 'true':
+    train(args)
+
+if args.test is 'true':
+    test(args)
+
+print('[*] OVER')
--- trainer.py
+++ trainer.py
@@ -8,118 +8,120 @@ from model import collate_fn, build_pretrained_model
 from model import RefScorer
 from model import UnrefScorer
 
-##argument
-device = torch.device('cuda:3')
-ninput = 150 #embeddingsize
-nlayer = 1
-nbatch = 1028
-nhidden= 512
-margin = 1
-epoch  = 50
-learningrate = 0.001
+def train(args):
+    print('[*] TRAIN')
 
-##dataset
-dataset_path = 'dataset'
-gold_reply_path = os.path.join(dataset_path, 'test_reply_sampled.txt')
-gene_reply_path = os.path.join(dataset_path, 'gene_reply_sampled.txt')
-train_src_path  = os.path.join(dataset_path, 'src_train.txt')
-train_tar_path  = os.path.join(dataset_path, 'tar_train.txt')
-test_query_path = os.path.join(dataset_path, 'test_query_sampled.txt')
+    device = torch.device('cuda:'+str(args.device))
+    ninput = args.dim
+    nlayer = args.layer
+    nbatch = args.batch
+    nhidden= args.hidden
+    margin = args.margin
+    epoch  = args.epoch
+    learningrate = args.lr
+    dataset_path = args.data
+    onmt_path = args.pretrain
 
-refer_test_data_path_list  = [gene_reply_path, gold_reply_path]
-unref_train_data_path_list = [train_src_path, train_tar_path]
-unref_test_data_path_list  = [test_query_path, gold_reply_path]
+    gold_reply_path = os.path.join(dataset_path, 'test_reply_sampled.txt')
+    gene_reply_path = os.path.join(dataset_path, 'gene_reply_sampled.txt')
+    train_src_path  = os.path.join(dataset_path, 'src_train.txt')
+    train_tar_path  = os.path.join(dataset_path, 'tar_train.txt')
+    test_query_path = os.path.join(dataset_path, 'test_query_sampled.txt')
 
-vocab_path = os.path.join('sample_onmt', 'sample.vocab.pt')
+    #refer_test_data_path_list  = [gene_reply_path, gold_reply_path]
+    unref_train_data_path_list = [train_src_path, train_tar_path]
+    #unref_test_data_path_list  = [test_query_path, gold_reply_path]
+    vocab_path = os.path.join(onmt_path, 'sample.vocab.pt')
 
-#pre-trained model
-onmt_path = 'sample_onmt'
-onmt_vocab_path = os.path.join(onmt_path, 'sample.vocab.pt')
-onmt_model_path = os.path.join(onmt_path, 'sample.model.pt')
+    #pre-trained model
+    #onmt_vocab_path = os.path.join(onmt_path, 'sample.vocab.pt')
+    #onmt_model_path = os.path.join(onmt_path, 'sample.model.pt')
 
-#data load
-unref_train_dataset = Dataset(vocab_path = vocab_path, data_path_list = unref_train_data_path_list, max_length = 50)
-unref_nega_dataset  = NegativeDataset(unref_train_dataset.data, unref_train_dataset.vocab)
-print('positive ', unref_train_dataset[0], unref_train_dataset.data[0])
-#print('negative ', negative_dataset[0], negative_dataset.data[0]) 
+    #data load
+    unref_train_dataset = Dataset(vocab_path = vocab_path, data_path_list = unref_train_data_path_list, max_length = 50)
+    unref_nega_dataset  = NegativeDataset(unref_train_dataset.data, unref_train_dataset.vocab)
+    #print('positive ', unref_train_dataset[0], unref_train_dataset.data[0])
+    #print('negative ', negative_dataset[0], negative_dataset.data[0]) 
 
-positive_loader = DataLoader(dataset = unref_train_dataset, batch_size = nbatch, collate_fn = collate_fn, num_workers = 8)
-negative_loader = DataLoader(dataset = unref_nega_dataset,  batch_size = nbatch, collate_fn = collate_fn, num_workers = 8)
+    positive_loader = DataLoader(dataset = unref_train_dataset, batch_size = nbatch, collate_fn = collate_fn, num_workers = 8)
+    negative_loader = DataLoader(dataset = unref_nega_dataset,  batch_size = nbatch, collate_fn = collate_fn, num_workers = 8)
 
-vocab_size= unref_train_dataset.getVocabSize()
-batch_num = math.ceil(unref_train_dataset.getInstanceSize() / nbatch )
-print('[*] # of batch: ', batch_num, ' pos, neg :', len(positive_loader), len(negative_loader))
+    vocab_size= unref_train_dataset.getVocabSize()
+    batch_num = math.ceil(unref_train_dataset.getInstanceSize() / nbatch )
+    print('[*] # of batch: ', batch_num, ' pos, neg :', len(positive_loader), len(negative_loader))
 
-#ninput, nhidden, nlayer, ntoken, nbatch
-unrefer_pos_model = UnrefScorer(ninput, nhidden, nlayer, vocab_size, nbatch, device)
-unrefer_pos_model = unrefer_pos_model.to(device)
+    #ninput, nhidden, nlayer, ntoken, nbatch
+    unrefer_pos_model = UnrefScorer(ninput, nhidden, nlayer, vocab_size, nbatch, device)
+    unrefer_pos_model = unrefer_pos_model.to(device)
 
-unrefer_neg_model = UnrefScorer(ninput, nhidden, nlayer, vocab_size, nbatch, device)
-unrefer_neg_model = unrefer_neg_model.to(device)
+    unrefer_neg_model = UnrefScorer(ninput, nhidden, nlayer, vocab_size, nbatch, device)
+    unrefer_neg_model = unrefer_neg_model.to(device)
 
-loss_f = torch.nn.MarginRankingLoss(margin)
-optimizer1 = torch.optim.Adam(unrefer_pos_model.parameters(), lr = learningrate)
-optimizer2 = torch.optim.Adam(unrefer_neg_model.parameters(), lr = learningrate)
-total_loss = 0
-
-for i in range(epoch): #epoch
-    iter_positive = iter(positive_loader)
-    iter_negative = iter(negative_loader)
-    for mini in range(batch_num):
-        #positive training
-        pos_src, pos_src_len, pos_tar, pos_tar_len = next(iter_positive)
-        neg_src, neg_src_len, neg_tar, neg_tar_len = next(iter_negative)
-        #print('pos', pos_src, '/', pos_src_len, '/', pos_tar, '/', pos_tar_len)
-         
-        encd_pos = unrefer_pos_model(pos_src, pos_src_len, pos_tar, pos_tar_len)
-        #encd_neg = unrefer_pos_model(neg_src, neg_src_len, neg_tar, neg_tar_len)
-        encd_neg = unrefer_neg_model(neg_src, neg_src_len, neg_tar, neg_tar_len)
-        #print('next ', mini, encd_pos.size(), encd_neg.size())
-        
-        target = torch.ones(encd_pos.size(0), 1).to(device)#batch
-        
-        loss = loss_f(encd_pos, encd_neg, target)
-        total_loss =total_loss+ loss.item()
-        unrefer_pos_model.zero_grad()
-        unrefer_neg_model.zero_grad()
-        loss.backward()
-        optimizer1.step()
-        optimizer2.step()
-
-    print('[-] epoch: ', i, ', total_loss :', total_loss)
+    loss_f = torch.nn.MarginRankingLoss(margin)
+    optimizer1 = torch.optim.Adam(unrefer_pos_model.parameters(), lr = learningrate)
+    optimizer2 = torch.optim.Adam(unrefer_neg_model.parameters(), lr = learningrate)
     total_loss = 0
 
-torch.save(unrefer_pos_model, 'result/positive_model.th')
-torch.save(unrefer_neg_model, 'result/negative_model.th')
+    for i in range(epoch): #epoch
+        iter_positive = iter(positive_loader)
+        iter_negative = iter(negative_loader)
+        for mini in range(batch_num):
+            #positive training
+            pos_src, pos_src_len, pos_tar, pos_tar_len = next(iter_positive)
+            neg_src, neg_src_len, neg_tar, neg_tar_len = next(iter_negative)
+            #print('pos', pos_src, '/', pos_src_len, '/', pos_tar, '/', pos_tar_len)
+         
+            encd_pos = unrefer_pos_model(pos_src, pos_src_len, pos_tar, pos_tar_len)
+            encd_neg = unrefer_neg_model(neg_src, neg_src_len, neg_tar, neg_tar_len)
+            #print('next ', mini, encd_pos.size(), encd_neg.size())
+        
+            target = torch.ones(encd_pos.size(0), 1).to(device)#batch
+        
+            loss = loss_f(encd_pos, encd_neg, target)
+            total_loss =total_loss+ loss.item()
+            unrefer_pos_model.zero_grad()
+            unrefer_neg_model.zero_grad()
+            loss.backward()
+            optimizer1.step()
+            optimizer2.step()
 
-#unrefer_pos_model = torch.load('result/positive_model.th')
-#unrefer_neg_model = torch.load('result/negative_model.th')
+        print('[-] epoch: ', i, ', total_loss :', total_loss)
+        total_loss = 0
 
-unref_test_dataset = Dataset(vocab_path = vocab_path,data_path_list = unref_test_data_path_list, max_length = 50)
-unref_test_loader  = DataLoader(dataset = unref_test_dataset, batch_size = nbatch, collate_fn = collate_fn, num_workers= 8)
+    torch.save(unrefer_pos_model, args.output+'_pos.th')
+    torch.save(unrefer_neg_model, args.output+'_neg.th')
 
-#pretrained = build_pretrained_model(onmt_path, unref_test_dataset.vocab)
-#ref_model = RefScorer(pretrained, device)
-#print('refer model load')
+def test(args):
 
-#test
-positive_test = open('result/positive_result.txt', 'w', encoding = 'utf-8')
-negative_test = open('result/negative_result.txt', 'w', encoding = 'utf-8')
+    print('[*] TEST')
 
-for query, q_len, reply, r_len in unref_test_loader:
-    prediction1 = unrefer_pos_model(query, q_len, reply, r_len)
-    prediction2 = unrefer_neg_model(query, q_len, reply, r_len)
-    #print(query,'/', q_len, '/', reply, '/', r_len)
-    print(prediction1)
-    print(prediction2)
-    positive_test.write(str(prediction1.data))
-    negative_test.write(str(prediction2.data))
-    print('break') 
-    break
+    unrefer_pos_model = torch.load(args.'_pos.th')
+    unrefer_neg_model = torch.load('_neg.th')
 
-positive_test.close()
-negative_test.close()
+    unref_test_dataset = Dataset(vocab_path = vocab_path,data_path_list = unref_test_data_path_list, max_length = 50)
+    unref_test_loader  = DataLoader(dataset = unref_test_dataset, batch_size = nbatch, collate_fn = collate_fn, num_workers= 8)
 
+    #pretrained = build_pretrained_model(onmt_path, unref_test_dataset.vocab)
+    #ref_model = RefScorer(pretrained, device)
+    #print('refer model load')
+
+    #test
+    positive_test = open('result/positive_result.txt', 'w', encoding = 'utf-8')
+    negative_test = open('result/negative_result.txt', 'w', encoding = 'utf-8')
+
+    for query, q_len, reply, r_len in unref_test_loader:
+        prediction1 = unrefer_pos_model(query, q_len, reply, r_len)
+        prediction2 = unrefer_neg_model(query, q_len, reply, r_len)
+        #print(query,'/', q_len, '/', reply, '/', r_len)
+        print(prediction1)
+        print(prediction2)
+        positive_test.write(str(prediction1.data))
+        negative_test.write(str(prediction2.data))
+        print('break') 
+        break
+
+    positive_test.close()
+    negative_test.close()
 
 '''
 ##reference score 
